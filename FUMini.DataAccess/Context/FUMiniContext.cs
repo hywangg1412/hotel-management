9bb08ecd33b11131c5731eaa4ced5f8bd84f68e6
huydev <hyperrhy1412@gmail.com>
2025-07-04 16:57:34 +0700

    feat: add FUMiniContext and configure EF Core context

---
+++ FUMini.DataAccess/Context/FUMiniContext.cs
@@ -0,0 +1,41 @@
+﻿using FUMini.BussinessObjects.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace FUMini.DataAccess.Context
+{
+    public class FUMiniContext : DbContext
+    {
+        public FUMiniContext() { }
+
+        public FUMiniContext(DbContextOptions options) : base(options) { }
+
+        private string GetConnectionString()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            return configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(GetConnectionString());
+        }
+
+        // DBSet properties
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<BookingReservation> BookingReservations { get; set; }
+        public DbSet<BookingDetail> BookingDetails { get; set; }
+        public DbSet<RoomInformation> RoomInformations { get; set; }
+        public DbSet<RoomType> roomTypes { get; set; }
+
+        // Configure relationship
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(FUMiniContext).Assembly);
+        }
+    }
+}
